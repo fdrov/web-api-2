85e6e8c99b2c511b52280c3846489ceb37f4a508
Anton <oldgot@gmail.com>
2024-11-16 13:51:26 +0300

    переделана логика работы is_shorten_link

--- main.py
+++ main.py
@@ -4,66 +4,63 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 import requests
 
-load_dotenv()
-
-token = os.environ['TOKEN']
-
 
 def shorten_link(token: str, url: str) -> str:
-    api_method = 'https://api.vk.ru/method/utils.getShortLink'
+    api_method = "https://api.vk.ru/method/utils.getShortLink"
     params = {
-        'v': '5.199',
-        'access_token': token,
-        'url': url,
+        "v": "5.199",
+        "access_token": token,
+        "url": url,
     }
     response = requests.get(url=api_method, params=params)
     response.raise_for_status()
     parsed_response = response.json()
-    if 'error' in parsed_response:
-        raise ValueError(parsed_response['error']['error_msg'])
-    short_link = parsed_response['response']['short_url']
+    if "error" in parsed_response:
+        raise ValueError(parsed_response["error"]["error_msg"])
+    short_link = parsed_response["response"]["short_url"]
     return short_link
 
 
 def count_clicks(token, link):
-    api_method = 'https://api.vk.ru/method/utils.getLinkStats'
-    path = urlparse(link).path.strip('/')
-    params = {
-        'v': '5.199',
-        'access_token': token,
-        'key': path,
-        'interval': 'forever'
-    }
+    api_method = "https://api.vk.ru/method/utils.getLinkStats"
+    path = urlparse(link).path.strip("/")
+    params = {"v": "5.199", "access_token": token, "key": path, "interval": "forever"}
     response = requests.get(url=api_method, params=params)
     response.raise_for_status()
-    result = response.json()
-    if 'error' in result:
-        raise ValueError(result['error']['error_msg'])
-    clicks_count = result['response']['stats'][0]['views']
+    parsed_response = response.json()
+    if "error" in parsed_response:
+        raise ValueError(parsed_response["error"]["error_msg"])
+    clicks_count = parsed_response["response"]["stats"][0]["views"]
     return clicks_count
 
 
-def is_shorten_link(url):
-    site = urlparse(url).netloc
-    if site == 'vk.cc':
-        return True
-    return False
+def is_shorten_link(token, url):
+    api_method = "https://api.vk.ru/method/utils.getLinkStats"
+    path = urlparse(url).path.strip("/")
+    params = {"v": "5.199", "access_token": token, "key": path, "interval": "forever"}
+    response = requests.get(url=api_method, params=params)
+    response.raise_for_status()
+    parsed_response = response.json()
+    return not "error" in parsed_response
 
 
 def main():
+    load_dotenv()
+    vk_service_token = os.environ["VK_SERVICE_TOKEN"]
+
     try:
-        url = input('Type your link:\n')
-        if is_shorten_link(url):
-            print('Кликов: ', count_clicks(token, url))
+        url = input("Type your link:\n")
+        if is_shorten_link(vk_service_token, url):
+            print("Кликов: ", count_clicks(vk_service_token, url))
         else:
-            print('Сокращенная ссылка: ', shorten_link(token, url))
+            print("Сокращенная ссылка: ", shorten_link(vk_service_token, url))
     except requests.exceptions.HTTPError as err:
-        print('There is an HTTP error:', err)
+        print("Произошла HTTP ошибка:", err)
     except ValueError as err:
-        print('ValueError', err)
+        print("Проверьте правильность ссылки:", err)
     except IndexError:
-        print('Please, retry the request')
+        print("Повторите запрос")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
